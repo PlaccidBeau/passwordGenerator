c3d1c43004522918efa9ed2f339bcc5eda6910b3
Eric Beauford <122799262+PlaccidBeau@users.noreply.github.com>
2024-02-28 17:33:58 +0000

    updated test_password.rb path and added assert

--- tests/test_password.rb
+++ tests/test_password.rb
@@ -1,11 +1,12 @@
 require 'minitest/autorun'
 # require '../app.rb'
-require "../password"
-require "../passwordCreate"
+require "./password"
+require "./passwordCreate"
 
 class TestPassword < Minitest::Test
   def test_password
     runPassword = Passwordcreate.new
     runPassword.run
+    assert(runPassword, msg = nil)
   end
 end
